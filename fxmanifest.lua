0674c963604003aaa5377f1e22880c509a0b294b
TheFlyBandit <102429379+TheFlyBandit@users.noreply.github.com>
2023-02-10 17:49:44 -0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+
+fx_version 'cerulean'
+game { 'gta5' }
+author 'TheFlyBandit'
+description 'Crenshaw Fitted Addon Hat'
+discord 'https://discord.gg/MH9BADQuZy'
+
+files {
+  'mp_m_freemode_01_mp_m_theflybandit.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_theflybandit.meta'
